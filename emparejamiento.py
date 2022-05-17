278b3bb6e5549471927ba08b363ee17cf0c9c973
migdlp <73550982+migdlp@users.noreply.github.com>
2022-05-17 18:05:02 +0200

    Update emparejamiento.py

--- emparejamiento.py
+++ emparejamiento.py
@@ -14,7 +14,6 @@ def main():
 	# creamos las N respuestas de 128bits a partir de N sets de 64 challenges de 64bits 
 	# (cada challenge da una respuesta de 1 bit, por lo que resultan respuestas de 64 bits)
 	for i in range(0 ,N ):
-		print(i)
 
 		# n = challenge length, N = response length
 		challenges = random_inputs(n=64, N=64, seed=i)
@@ -25,20 +24,17 @@ def main():
 		# transforma array de 1s y -1s a string de 0s y 1s
 		response = "".join(list(map(transform_array, response)))
 		responseBytes = int(response, 2).to_bytes(len(response) // 8, byteorder='big')
-		print(response)
 
-    	# HASH
+
+    		# HASH
 		hash_obj = SHA3_256.new(responseBytes)
 		responseHash = hash_obj.hexdigest()
-		print(responseHash)
+
 
 		# escribimos (en modo adición) cada hash
 		with open('BBDD_CRPs.txt', 'a') as f:
 			f.write(responseHash+"\n")
-		
-
-
-		
+			
 
 #Change every bit of the array to be a string,
 # and if it's -1 change it to 0
